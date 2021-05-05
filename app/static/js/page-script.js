77cbf42bab980f354bb2fd481dc2c7bed759ab09
滑板鞋 <aijm73@gmail.com>
2021-05-05 14:04:56 +0800

    Wrapped content and header into grid

--- app/static/js/page-script.js
+++ app/static/js/page-script.js
@@ -1,18 +1,19 @@
-/* JavaScript for the topbar */
+/* JavaScript for the header */
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
 // Get the topbar
-var topbar = document.getElementById("topbar");
+// var topbar = document.getElementById("topbar");
 
 // Get the offset position of the topbar
-var sticky = topbar.offsetTop;
+// var sticky = topbar.offsetTop;
 
 // Add the sticky class to the topbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+/* function myFunction() {
   if (window.pageYOffset >= sticky) {
     topbar.classList.add("sticky")
   } else {
     topbar.classList.remove("sticky");
   }
-}
+} */
+
