0747481ad30aa19a753c7d2933c75a6c8864ca0b
lmbhov <lmbhov@gmail.com>
2021-05-05 14:43:42 +0800

    completed test of adding new user

--- testing.py
+++ testing.py
@@ -11,21 +11,22 @@ from app import app, db
 #Import more tables as needed
 from app.models import Scores, User
 
-
-
-
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class Tester(unittest.TestCase):
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     driver = None
 
     def setUp(self):    
         #Creates the testing envrionment by clearing and filling the database
         #and launching a chrome session to test in.
+
         chrome_driver_path = "chromedriver.exe"
 
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
         # create a new Chrome session
-        self.driver = webdriver.Chrome(chrome_driver_path)
+        self.driver = webdriver.Chrome(chrome_driver_path, options=options)
 
         if not self.driver:
             self.skipTest("No Web Browser Found")
@@ -56,21 +57,16 @@ class Tester(unittest.TestCase):
             db.session.commit()
             db.session.remove()
 
-    def test_sanity(self):
-        #Remove this test
-        self.assertEqual(1, 1)
-
     def test_user_exists(self):
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+
         #A simple test to determine database is functional, and can be accessesed
         u1 = User.query.get(1)
         self.assertEqual(u1.username, "real_person")
 
     def test_new_user(self):
         #Tests the process of creating a new user, then signing in. 
-
-        #TODO Test signin
-        #TODO test if password is hashed
-        #TODO Make test functional
+        
         self.driver.get("http://127.0.0.1:5000/register")
         self.driver.implicitly_wait(5)
         self.driver.find_element_by_id("username").send_keys("Gary")
@@ -86,8 +82,24 @@ class Tester(unittest.TestCase):
 
         time.sleep(1)
         self.driver.implicitly_wait(20)
-        self.assertEqual(1, 1)
+        
+        
+        self.driver.refresh()    
+        self.driver.find_element_by_id("login").click()
+        self.driver.find_element_by_id("email").send_keys("Gary@email.com")
+        self.driver.find_element_by_id("password").send_keys("pAsSwOrD")
+        
+        time.sleep(1)
+        self.driver.implicitly_wait(20)
 
+        
+        self.driver.find_element_by_id("submit").click()
+        
+        self.assertEqual(self.driver.find_element_by_xpath('//li[@id="login"]').get_attribute('innerHTML'), "Logout", "Should be signed in")
+
+
+        
+    #TODO test if password is hashed
     #Add more tests here
 
 
