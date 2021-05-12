7d333d91f33521ab9a88fad25857b297598d73f7
lmbhov <lmbhov@gmail.com>
2021-05-12 18:37:05 +0800

    added 403 and 500 errors

--- app/handler.py
+++ app/handler.py
@@ -4,3 +4,12 @@ from flask import Flask, render_template
 @app.errorhandler(404)
 def error_404(error):
     return render_template("errors/404.html"), 404
+
+@app.errorhandler(500)
+def error_505(error):
+    db.session.rollback()
+    return render_template('errors/500.html'), 500
+
+@app.errorhandler(403)
+def error_403(error):
+    return render_template('errors/403.html'), 403
